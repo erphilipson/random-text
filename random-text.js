4a004a1adf6b7b7b201e57701cea9f21cf46e05a
Ethan Philipson <erphilipson@gmail.com>
2017-06-19 13:36:21 -0400

    Remove node modules

--- random-text.js
+++ random-text.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const loremIpsum = require('lorem-ipsum');
 
+app.get('/', function(req, res){
+  res.send('Nothing to see here! HINT: Try /lorem');
+})
+
 app.get('/lorem', function (req, res){
   res.send(loremIpsum({count: 3, units: 'paragraphs', format: 'html'}));
 })
